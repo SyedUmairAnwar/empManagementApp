09361b3fcc5c126cf6b2af92a7dfbb48a870bd12
syed_umair98 <syed.umair.anwar@antronsys.com>
2020-12-30 08:55:50 +0530

    Added:text validation

--- js/EmpManage.js
+++ js/EmpManage.js
@@ -3,3 +3,11 @@ const op=document.querySelector('.salary-output')
 salary.addEventListener("input",function(){
     op.textContent=salary.value;
 })
+const name1=document.querySelector("#name")
+const text_error=document.querySelector(".text-error")
+name1.addEventListener("input",function(){
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if(nameRegex.test(name1.value)){
+        text_error.textContent=""
+    }else text_error.textContent="Name is incorrect"
+})
